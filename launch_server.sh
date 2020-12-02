b546794f78337967c825add356449f0250d80559
Montia <583736870@qq.com>
2020-12-02 16:09:33 +0800

    debug launch_server.sh

--- launch_server.sh
+++ launch_server.sh
@@ -1,10 +1,10 @@
 #/usr/bin/sh
 export FLASK_APP=$(dirname $0)/server
 export FLASK_ENV=development
-if [ $# -gt 0 -a $1 == "init-db" ]
+if [ $# > 0 -a "$1"x == "init-db"x ];
 then
 	flask init-db
-elif [ $# -gt 0 ]
+elif [ $# -gt 0 ];
 then
 	echo "Error, only support argument \"init-db\" or no argument"
 	exit
