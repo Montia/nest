60516dce07226394f0e4cc4e28a7006ebf8a3015
Montia <583736870@qq.com>
2021-01-01 20:31:08 +0800

    s

--- server/nest.py
+++ server/nest.py
@@ -8,13 +8,13 @@ def test_lab(lab, zipfile):
     # check if params are valid
     if type(lab) != int or lab < 1 or lab > 7:
         score = 0
-        log = '<h4>Lab{} is invalid</h4>'.format(lab)
-        return score, log.decode('utf-8')
+        test_result = '<h4>Lab{} is invalid</h4>'.format(lab)
+        return score, test_result.decode('utf-8')
 
     if type(zipfile) != str or not zipfile.endswith('.zip'):
         score = 0
-        log = '<h4>Uploaded file {} is not a ".zip" file</h4>'.format(lab)
-        return score, log.decode('utf-8')
+        test_result = '<h4>Uploaded file {} is not a ".zip" file</h4>'.format(lab)
+        return score, test_result.decode('utf-8')
 
     # make the directory for the uploaded code, where unzip inflates to 
     dest_dir = os.path.abspath(os.path.dirname(zipfile))
@@ -26,50 +26,50 @@ def test_lab(lab, zipfile):
         pass
     os.mkdir(inflated_dir)
     ret = os.system('unzip {} -d {} > {} 2>&1'.format(\
-        zipfile, inflated_dir, os.path.join(dest_dir, 'unzip.result')))
+        zipfile, inflated_dir, os.path.join(dest_dir, 'unzip.output')))
     if ret != 0:
         score = 0
-        log = '<h4>Unzip failed, ret = {}</h4>'.format(ret)
-        log += 'Command unzip\'s output: <br/>'
-        with open(os.path.join(dest_dir, 'unzip.result')) as f:
+        test_result = '<h4>Unzip failed, ret = {}</h4>'.format(ret)
+        test_result += 'Command unzip\'s output: <br/>'
+        with open(os.path.join(dest_dir, 'unzip.output')) as f:
             for line in f.readlines():
                 if line.startswith('note:'):
                     break
-                log += line.replace('\n', '<br/>')
-        return score, log.decode('utf-8')
+                test_result += line.replace('\n', '<br/>')
+        return score, test_result.decode('utf-8')
 
     # check whether the uploaded code contains sole directory
     inflated_files = os.listdir(inflated_dir)
     if len(inflated_files) > 1 or inflated_files[0] != uploaded_root_dirname \
         or not os.path.isdir(os.path.join(inflated_dir, uploaded_root_dirname)):
         score = 0
-        log = '<h4>The uploaded zip file should contain \
+        test_result = '<h4>The uploaded zip file should contain \
              only a directory named "nest". </h4>'
-        log += 'Your zip file contain directory/files as follows: <br/>'
+        test_result += 'Your zip file contain directory/files as follows: <br/>'
         for inflated_file in inflated_files:
             if os.path.isdir(os.path.join(inflated_dir, inflated_file)):
                 file_type = 'directory'
             else:
                 file_type = 'not directory'
-            log += '{} ({}) <br/>'.format(inflated_file, file_type)
+            test_result += '{} ({}) <br/>'.format(inflated_file, file_type)
 
-        return score, log.decode('utf-8')
+        return score, test_result.decode('utf-8')
     
     # check whether the code contains "code" directory
     uploaded_root_dir = os.path.join(inflated_dir, uploaded_root_dirname)
     code_dir = os.path.join(uploaded_root_dir, 'code')
     if not os.path.isdir(code_dir):
         score = 0
-        log = '<h4>Your "{}" doesn\'t contain "code" directory. \
+        test_result = '<h4>Your "{}" doesn\'t contain "code" directory. \
             </h4>'.format(uploaded_root_dirname)
-        log += 'Your zip file contain directory/files as follows: <br/>'
+        test_result += 'Your zip file contain directory/files as follows: <br/>'
         for inflated_file in inflated_files:
             if os.path.isdir(os.path.join(inflated_dir, inflated_file)):
                 file_type = 'directory'
             else:
                 file_type = 'not directory'
-            log += '{} ({}) <br/>'.format(inflated_file, file_type)
-        return score, log.decode('utf-8')
+            test_result += '{} ({}) <br/>'.format(inflated_file, file_type)
+        return score, test_result.decode('utf-8')
 
     # copy/overwrite directory "nest" and "machine"  
     nest_dir = os.path.join(code_dir, 'nest')
@@ -95,50 +95,99 @@ def test_lab(lab, zipfile):
         lab_dir = os.path.join(nest_dir, 'lab{}'.format(lab))
         os.chdir(lab_dir)
         
-        # produce test results using Makefile
-        os.system('make depend && make nest > {} 2>&1'\
-            .format(os.path.join(dest_dir, 'make.result')))
-        if ret != 0:
-            score = 0
-            log = '<h4>Make failed, ret = {}</h4>'.format(ret)
-            log += 'Make\'s output: <br/>'
-            with open(os.path.join(dest_dir, 'make.result')) as f:
-                for line in f.readlines():
-                    # if line.startswith('note:'):
-                    #     break
-                    log += line.replace('\n', '<br/>')
-            return score, log.decode('utf-8')
-        log = ''
-        # log = 'lab{}: {}<br/>'.format(lab, os.getcwd())
-        # for name in os.listdir('.'):
-        #     log += '{} <br/>'.format(name)
-
         # parse and score the test results using scripts
         sys.path.append(os.getcwd())
+        sys.path.append(os.getcwd()+'/..')
         from score import tests
-        # log += 'tests: {}<br/>'.format(tests)
+
+        # produce test results using Makefile
+        if lab == 2:
+            for test in tests:
+                makefile_name = 'Makefile.{}'.format(test)
+                ret = os.system('make -f {0} clean && make -f {0} nest > {1} 2>&1'\
+                    .format(makefile_name, 'make_nest.result'))
+                if ret != 0:
+                    score = 0
+                    test_result = '<h4>Make failed, ret = {}</h4>'.format(ret)
+                    test_result += 'Make\'s output: <br/>'
+                    with open('make_nest.result') as f:
+                        for line in f.readlines():
+                            # if line.startswith('note:'):
+                            #     break
+                            test_result += line.replace('\n', '<br/>')
+                    return score, test_result.decode('utf-8')
+        else:
+            ret = os.system('make depend > {} 2>&1'\
+                .format(os.path.join(dest_dir, 'make_depend.result')))
+            if ret != 0:
+                score = 0
+                test_result = '<h4>Make depend failed, ret = {}</h4>'.format(ret)
+                test_result += 'Make\'s output: <br/>'
+                with open(os.path.join(dest_dir, 'make_depend.result')) as f:
+                    for line in f.readlines():
+                        # if line.startswith('note:'):
+                        #     break
+                        test_result += line.replace('\n', '<br/>')
+                return score, test_result.decode('utf-8')
+            # produce test results using Makefile
+            ret = os.system('make nest > {} 2>&1'\
+                .format(os.path.join(dest_dir, 'make_nest.result')))
+            if ret != 0:
+                score = 0
+                test_result = '<h4>Make failed, ret = {}</h4>'.format(ret)
+                test_result += 'Make\'s output: <br/>'
+                with open(os.path.join(dest_dir, 'make_nest.result')) as f:
+                    for line in f.readlines():
+                        # if line.startswith('note:'):
+                        #     break
+                        test_result += line.replace('\n', '<br/>')
+                return score, test_result.decode('utf-8')
+        
+        test_result = ''
         score = 0
         for test in tests:
+            if test.endswith('kernel'):
+                ret = os.system('./nachos_test_{0} -d > {0}.result 2>&1'.format(test))
+            elif test.endswith('user'):
+                ret = os.system('./nachos_test_{0} -d -x {0}.noff > {0}.result 2>&1'.format(test))
+            else:
+                test_result += '<b>Test {} failed.<br/>{}<br/></b>'.format(test, 'Only support cases ending with"kernel" and "user"')
+                test_result += '<br/>'
+                continue
+            if ret != 0:
+                test_result += '<b>Test {} failed.<br/>{}<br/></b>'.format(test, 'Runtime Error')
+                test_result += open(test+'.result').read().replace('\n', '<br/>')
+                test_result += '<br/>'
+                continue
+
             exec 'from {} import check'.format(test)
-            # log += test+'.result\'s output is as follows:<br/>'
+            # test_result += test+'.result\'s output is as follows:<br/>'
             with open(test+'.result') as f:
                 lines = f.readlines()
             passed, error_message = check(lines)
             if passed is True:
                 score += tests[test]
-                log += '<br/>Test {} passed, score: {}.'.format(test, tests[test])
+                test_result += '<b>Test {} passed, score: {}.<br/></b>'.format(test, tests[test])
+            else:
+                error_message = error_message.strip('\n')
+                test_result += '<b>Test {} failed.<br/>{}<br/></b>'.format(test, error_message)
+                with open(test+'.result') as f:
+                    for line_num, line in enumerate(f.readlines()):
+                        test_result += '{}\t{}'.format(line_num+1, line).replace('\n', '<br/>')
+                test_result += '<br/>'
 
     except Exception as e:
-        log = '<h4>Exception raised while testing.</h4>'
+        test_result = '<h4>Exception raised while testing.</h4>'
         exception_message = traceback.format_exc()
-        log += exception_message.replace('\n', '<br/>')
+        test_result += exception_message.replace('\n', '<br/>')
 
     finally:
-        try:
-            sys.path.remove(os.getcwd())
-        except ValueError as e:
-            pass
-        os.chdir(old_dir)
-        # log += 'Change directory back to {} <br/>'.format(os.getcwd())
+        if os.getcwd() != old_dir:
+            try:
+                sys.path.remove(os.getcwd())
+                sys.path.remove(os.getcwd()+'/..')
+            except ValueError as e:
+                pass
+            os.chdir(old_dir)
 
-    return score, log.decode('utf-8')
+    return score, test_result.decode('utf-8', "replace")
